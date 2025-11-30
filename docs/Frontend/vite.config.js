d65d1f6e176705beecca174483538393fac46409
suryasnataparhi10 <suryasnataparhi10@gmail.com>
2025-11-30 20:38:31 +0530

    Deploy React site

--- docs/Frontend/vite.config.js
+++ docs/Frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Funtoon_WORLD/docs/',
+  base: '/Funtoon_WORLD/', // <-- matches the repo name
   plugins: [react()],
-})
+  build: {
+    outDir: 'docs', // output build into docs folder
+    emptyOutDir: true,
+  },
+});
